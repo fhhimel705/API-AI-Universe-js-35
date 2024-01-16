0ecf78b8fa022d90911a2015b66db19b72f02a1a
FH Himel <fhhimel17@gmail.com>
2024-01-17 00:40:12 +0600

    functionality-added

--- js fiels/index.js
+++ js fiels/index.js
@@ -1,51 +1,73 @@
-fetch("https://openapi.programming-hero.com/api/ai/tools")
-  .then((res) => res.json())
-  .then((data) => {
-    // console.log(data.data.tools);
-    const aiData = data.data.tools;
-    const aiGridSection = document.getElementById("ai-grid-section");
-    aiData.forEach((aiCompany, index) => {
-      console.log(aiCompany.id);
-      const createAiDiv = document.createElement("div");
-      createAiDiv.classList.add("p-5");
-      createAiDiv.classList.add("border");
-      createAiDiv.classList.add("rounded-2xl");
-      createAiDiv.innerHTML = `
-        
-                <div class="border-b-2">
-                    <img  src="${
-                      aiCompany.image ? aiCompany.image : "ai.jpg"
-                    }" class="rounded-2xl" alt="">
-                <h5 class="text-2xl py-4 font-semibold">Features</h5>
-                <p class="text-lg text-slate-600 py-1">1. ${
-                  aiCompany.features[0]
-                }</p>
-                <p class="text-lg text-slate-600 py-1">2. ${
-                  aiCompany.features[1]
-                }</p>
-                <p class="text-lg text-slate-600 py-1 pb-6">3. ${
-                  aiCompany.features[2] ? aiCompany.features[2] : "That's it !"
-                }</p>
-                </div>
-                <div class="flex pt-5 justify-between">
-                    <div>
-                        <h4 class="text-2xl font-semibold  pb-2">${
-                          aiCompany.name
-                        }</h4>
-                        <p class="text-slate-600">Date : <span>11/01/2022</span> </p>
-                    </div>
-                    <div >
-                        <button  onclick="aiModal(${
-                          aiCompany.id
-                        })" class="bg-red-100  text-red-700 px-3  py-2 rounded-full"><i class="fa-solid fa-arrow-right"></i></button>
-                        
-                        
-</div>
-</div>
-        `;
-      aiGridSection.appendChild(createAiDiv);
+const showData = () => {
+  fetch("https://openapi.programming-hero.com/api/ai/tools")
+    .then((res) => res.json())
+    .then((data) => {
+
+      let aiData02 = data.data.tools;
+      displayData(aiData02 , 6);
+
+      const seeMore = document.getElementById("see-more-btn");
+      seeMore.classList.remove('hidden');
+      
+      seeMore.addEventListener("click", function () {
+        seeMore.classList.add('hidden');
+        displayData(aiData02);
+      });
     });
+};
+
+
+const displayData = (aiData02 , limit) => {
+
+const aiData =  aiData02.slice(0,limit);
+  
+  const aiGridSection = document.getElementById("ai-grid-section");
+  aiData.forEach((aiCompany) => {
+    console.log(aiCompany.id);
+    const createAiDiv = document.createElement("div");
+    createAiDiv.classList.add("p-5");
+    createAiDiv.classList.add("border");
+    createAiDiv.classList.add("rounded-2xl");
+    createAiDiv.innerHTML = `
+            
+                    <div class="border-b-2">
+                        <img  src="${
+                          aiCompany.image ? aiCompany.image : "ai.jpg"
+                        }" class="rounded-2xl" alt="">
+                    <h5 class="text-2xl py-4 font-semibold">Features</h5>
+                    <p class="text-lg text-slate-600 py-1">1. ${
+                      aiCompany.features[0]
+                    }</p>
+                    <p class="text-lg text-slate-600 py-1">2. ${
+                      aiCompany.features[1]
+                    }</p>
+                    <p class="text-lg text-slate-600 py-1 pb-6">3. ${
+                      aiCompany.features[2]
+                        ? aiCompany.features[2]
+                        : "That's it !"
+                    }</p>
+                    </div>
+                    <div class="flex pt-5 justify-between">
+                        <div>
+                            <h4 class="text-2xl font-semibold  pb-2">${
+                              aiCompany.name
+                            }</h4>
+                            <p class="text-slate-600">Date : <span>11/01/2022</span> </p>
+                        </div>
+                        <div >
+                            <button  onclick="aiModal(${
+                              aiCompany.id
+                            })" class="bg-red-100  text-red-700 px-3  py-2 rounded-full"><i class="fa-solid fa-arrow-right"></i></button>
+                            
+                            
+    </div>
+    </div>
+            `;
+    aiGridSection.appendChild(createAiDiv);
   });
+};
+
+
 
 const aiModal = (ID) => {
   console.log(ID);
@@ -60,44 +82,50 @@ const aiModal = (ID) => {
       const aiData = data.data;
 
       dialog.innerHTML = `
-      <div class="modal-box w-11/12 max-w-5xl px-16 py-8">
+      <div class="modal-box w-11/12 max-w-5xl md:px-16 md:py-8">
       <div class="grid md:grid-cols-2 gap-7 ">
           <div class="bg-red-50 border-2 rounded-xl border-red-400 p-4">
               <h5 class="text-xl font-semibold">${aiData.description}</h5>
              
 
               <div class=" grid grid-cols-3  gap-2 py-4">
-                  <p class="bg-white  text-green-500 font-semibold md:text-lg text-xs rounded-xl p-5">${
+                  <p class="bg-white flex  text-green-500 font-semibold md:text-lg text-center text-xs rounded-xl items-center  p-2 md:p-5">${
                     aiData.pricing[0].price
-                  } ${
-                    aiData.pricing[0].plan
-                  } </p>
-                  <p class="bg-white text-orange-500 font-semibold md:text-lg text-xs rounded-xl p-5">${
+                  } ${aiData.pricing[0].plan} </p>
+                  <p class="bg-white text-orange-500 flex font-semibold md:text-lg text-xs text-center items-center rounded-xl p-2 md:p-5">${
                     aiData.pricing[1].price
-                  } ${
-                    aiData.pricing[1].plan
-                  } </p>
-                  <p class="bg-white text-red-500 font-semibold md:text-lg text-xs rounded-xl p-5">${
+                  } ${aiData.pricing[1].plan} </p>
+                  <p class="bg-white text-red-500 flex font-semibold md:text-lg text-xs text-center items-center rounded-xl p-2 md:p-3">${
                     aiData.pricing[2].price
-                  } ${
-                    aiData.pricing[2].plan
-                  } </p>
+                  } ${aiData.pricing[2].plan} </p>
               </div>
               <div class="grid grid-cols-2">
                  <div>
                   <h6 class="text-xl font-bold">Features</h6>
                   <ul class = "pt-2">
-                      <li class="text-xs text-slate-500">. ${aiData.features[1].feature_name}</li>
-                      <li class="text-xs py-1 text-slate-500">. ${aiData.features[2].feature_name}</li>
-                      <li class="text-xs text-slate-500">. ${aiData.features[3].feature_name}</li>
+                      <li class="text-xs text-slate-500">. ${
+                        aiData.features[1].feature_name
+                      }</li>
+                      <li class="text-xs py-1 text-slate-500">. ${
+                        aiData.features[2].feature_name
+                      }</li>
+                      <li class="text-xs text-slate-500">. ${
+                        aiData.features[3].feature_name
+                      }</li>
                   </ul>
                  </div>
                  <div>
                   <h6 class="text-lg font-bold">Integrations</h6>
                   <ul class = "pt-2">
-                      <li class="text-xs text-slate-500">. ${aiData.integrations[0]}</li>
-                      <li class="text-xs py-1 text-slate-500">. ${aiData.integrations[1]}</li>
-                      <li class="text-xs text-slate-500">. ${aiData.integrations[2]}</li>
+                      <li class="text-xs text-slate-500">. ${
+                        aiData.integrations[0]
+                      }</li>
+                      <li class="text-xs py-1 text-slate-500">. ${
+                        aiData.integrations[1]
+                      }</li>
+                      <li class="text-xs text-slate-500">. ${
+                        aiData.integrations[2]
+                      }</li>
                   </ul>
                  </div>
               </div>
@@ -110,8 +138,12 @@ const aiModal = (ID) => {
                   ? aiData.image_link[0]
                   : aiData.image_link[1]
               }" alt="">
-              <h5 class="text-xl text-center py-8 font-bold">${aiData.input_output_examples[0].input}</h5>
-              <p class="text-xs text-center text-slate-700">${aiData.input_output_examples[0].output}</p>
+              <h5 class="text-xl text-center py-8 font-bold">${
+                aiData.input_output_examples[0].input
+              }</h5>
+              <p class="text-xs text-center text-slate-700">${
+                aiData.input_output_examples[0].output
+              }</p>
               </div>
 
 
@@ -128,3 +160,4 @@ const aiModal = (ID) => {
       `;
     });
 };
+showData();
